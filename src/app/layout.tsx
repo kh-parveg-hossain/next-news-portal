e2b9bd657b259290f352640f6234686f3d2d48d7
kh-parveg-hossain <parevgkh64@gmail.com>
2025-03-19 12:48:15 +0530

    change 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import { ThemeProvider } from "@/context/themeContext";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         <ThemeProvider>
         <Analytics/>
+        <SpeedInsights/>
         <Navbar/>
         <main className=" min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 dark:bg-gray-900">{children}</main>
        <Footer/>
